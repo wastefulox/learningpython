80f43e9d79bde74d6093e7de71f6ded5baad6718
wastefulox <mathew.sturdevant@my.strayer.edu>
2018-11-21 23:02:52 -0500

    08.82 - learning to use constructors in classes

--- 08.oop.py
+++ 08.oop.py
@@ -1,40 +1,63 @@
 # Chapter 8 - Object Oriented Programming
 
 # 08.81 - The First Steps in Object Orientation
-students = ["Max", "Monika"]
-students.append("Eric")
-print(students)
+# students = ["Max", "Monika"]
+# students.append("Eric")
+# print(students)
+#
+# class Student():
+#     def getName(self):
+#         print(self.firstname + " " + self.lastname)
+#
+# eric = Student()
+# eric.firstname = "Eric"
+# eric.lastname = "Miller"
+#
+# monica = Student()
+# monica.firstname = "Monika"
+# monica.lastname = "Fischer"
+#
+# class Company():
+#     def getName(self):
+#         print(self.legalName + " " + self.legalType)
+#
+# c = Company()
+# c.legalName = "MTJ Design"
+# c.legalType = "LLC"
+#
+# c.getName()
+# eric.getName()
+# monica.getName()
+#
+# def name5x(o):
+#     o.getName()
+#     o.getName()
+#     o.getName()
+#     o.getName()
+#     o.getName()
+#
+# name5x(eric)
+# name5x(c)
 
-class Student():
-    def getName(self):
-        print(self.firstname + " " + self.lastname)
+# 08.82 - Object Orientation - Constructors and Changing Properties
+# how do we make sure every object has all required attributes - use constructors!
 
-eric = Student()
-eric.firstname = "Eric"
-eric.lastname = "Miller"
+# class Student():
+#     # this constructor ensures all attributes and will error out if it is missing an attribute.
+#     def __init__(self, firstName, lastName, term):
+#         self.firstname = firstName
+#         self.lastname = lastName
+#         self.term = term
+#     def increaseTerm(self):
+#         self.term = self.term + 1
+#     def getInfo(self):
+#         print(self.firstname + " " + self.lastname + ", Semester: " + str(self.term))
+#
+# eric = Student("Eric", "Miller", 3)
+# eric.increaseTerm()
+# eric.getInfo()
+#
+# monica = Student("Monika", "Fischer", 5)
+# monica.getInfo()
 
-monica = Student()
-monica.firstname = "Monika"
-monica.lastname = "Fischer"
-
-class Company():
-    def getName(self):
-        print(self.legalName + " " + self.legalType)
-
-c = Company()
-c.legalName = "MTJ Design"
-c.legalType = "LLC"
-
-c.getName()
-eric.getName()
-monica.getName()
-
-def name5x(o):
-    o.getName()
-    o.getName()
-    o.getName()
-    o.getName()
-    o.getName()
-
-name5x(eric)
-name5x(c)
+# 08.84 -
