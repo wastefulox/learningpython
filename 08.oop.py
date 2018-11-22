2ec0f65ced23ea6b3318c12fecbe56f9b10cd8e7
wastefulox <mathew.sturdevant@my.strayer.edu>
2018-11-21 22:47:24 -0500

    into to classes

--- 08.oop.py
+++ 08.oop.py
@@ -2,3 +2,39 @@
 
 # 08.81 - The First Steps in Object Orientation
 students = ["Max", "Monika"]
+students.append("Eric")
+print(students)
+
+class Student():
+    def getName(self):
+        print(self.firstname + " " + self.lastname)
+
+eric = Student()
+eric.firstname = "Eric"
+eric.lastname = "Miller"
+
+monica = Student()
+monica.firstname = "Monika"
+monica.lastname = "Fischer"
+
+class Company():
+    def getName(self):
+        print(self.legalName + " " + self.legalType)
+
+c = Company()
+c.legalName = "MTJ Design"
+c.legalType = "LLC"
+
+c.getName()
+eric.getName()
+monica.getName()
+
+def name5x(o):
+    o.getName()
+    o.getName()
+    o.getName()
+    o.getName()
+    o.getName()
+
+name5x(eric)
+name5x(c)
