dd2590ab15b8949ab0623089d53d6fee3b9c227c
wastefulox <mathew.sturdevant@mail.strayer.edu>
2018-12-04 21:44:44 -0500

    build out initial framework for exercise 08.88

---
+++ 08.88 Inheritance Exercise.py
@@ -0,0 +1,23 @@
+# Task 1: complete the class, FileReader, so that when calling the lines() method, the file is accessed line by line. The lines() method should then return a list of the lines in the file.
+
+# Task 2: create the class, CsvReader, so that the FileReader is extended to read a csv file instead. It should return a multidimentional list.
+
+# Important: Leave the reading of the file to FileReader, and extend the lines() method in the CsvReader class by the functionality, which is needed, so that the multidimentional list is returned.
+
+class FileReader():
+    pass
+
+class CsvReader():
+    pass
+
+f = FileReader(./file.csv)
+print(f.lines())
+
+# expected output:
+# [surname, first_name, Doe, John, Miller, Monica]
+
+f = CsvReader("./file.csv")
+print(f.lines())
+
+# expected output
+# [['surname', 'first_name'], ['Doe', 'John'], ['Miller', 'Monica']]
